a7e316804d9c8119347e2d42b1514e6d111139cd
dan65577 <danielobazuaye9@gmail.com>
2026-02-20 16:04:10 +0000

    Fix Firebase v12 compatibility: replace removed getReactNativePersistence with getAuth

--- mobile/src/services/firebase.js
+++ mobile/src/services/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDI8v91yBsdcBPmMymJMgMRO_00H_nhe54",
@@ -14,8 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
+export const auth = getAuth(app);
 
 export const db = getFirestore(app);
